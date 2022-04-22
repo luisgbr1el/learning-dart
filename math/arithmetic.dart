caad07cdd88a74a62438a944c989fd4fce08daca
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-04-22 10:13:25 -0300

    Create arithmetic.dart
    This file contains the basics of arithmetic: addition, subtraction, multiplication and division in Dart.

---
+++ math/arithmetic.dart
@@ -0,0 +1,26 @@
+main() {
+  var valor1 = 60;
+  var valor2 = 10;
+  
+  print("Valor 1 = $valor1\nValor 2 = $valor2\n");
+  
+  // Soma
+  var soma = valor1 + valor2;
+  print("A soma entre eles é $soma");
+  
+  // Subtração
+  var sub = valor1 - valor2;
+  print("A subtração entre eles é $sub");
+  
+  // Multiplicação
+  var mult = valor1 * valor2;
+  print("A multiplicação entre eles é $mult");
+  
+  // Divisão
+  var div = valor1 / valor2;
+  print("A divisão entre eles é $div");
+  
+  // Resto da divisão
+  var resto = valor1 % valor2;
+  print("O resto da divisão entre eles é $resto");
+}
